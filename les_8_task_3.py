e3dde7539dac5e10b93ca15fbe37032ff4f936db
Arsenia Alekseeva <84538522+ArseniaA@users.noreply.github.com>
2022-02-11 17:37:12 +0300

    Update les_8_task_3.py

--- les_8_task_3.py
+++ les_8_task_3.py
@@ -3,14 +3,27 @@
 # a. граф должен храниться в виде списка смежности;
 # b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин.
 
-def G()
+import random
 
 
-visited = [False] * n
+def G(n):
+    G = {}
+    for i in range(n):
+        G[i] = set()
+        edges = random.randrange(0, n - 1)
+        while len(G[i]) < edges:
+            edge = random.randrange(0, n)
+            if edge != i:
+                G[i].add(edge)
+    return G
 
 
-def dfs(start, visited, G):
-    visited[start] = True
-    for u in G[start]:
-        if not visited[u]:
-            dfs(u, visited, G)
+def dfs(start, G):
+    is_visited = [False] * n
+    is_visited[start] = True
+
+    for i in G[start]:
+        if is_visited[i] == False:
+            dfs(i, G)
+
+G(5)
